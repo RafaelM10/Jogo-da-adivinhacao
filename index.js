e6219ebd6e82dd835d21e85a8d12237919512b16
meiradev <rafameira10@icloud.com>
2022-08-31 20:37:26 -0300

    update commit

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const randomNumber = Math.round(Math.random() * 10);
+const btnTry = document.getElementById("btnTry");
+const btnReset = document.getElementById("btnReset");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
 
+const randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1
 
-function handleClick(event) {
+
+function handleTryClick(event) {
   event.preventDefault()
 
   const inputNumber = document.getElementById("inputNumber")
   
   if(Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
+    screen1.classList.add("hide");
+    screen2.classList.remove("hide");
 
-    document.querySelector(".screen2 h2")
-    .innerText = `acertou em ${xAttempts}  tentativas`
+    screen2.querySelector(".screen2 h2").innerText = `acertou em ${xAttempts}  tentativas`
   }
 
+  inputNumber.value = ""
   xAttempts++
 }
+
+btnTry.addEventListener("click", handleTryClick)
+
+
+btnReset.addEventListener("click", function(){
+  screen1.classList.remove("hide");
+  screen2.classList.add("hide");
+
+  xAttempts = 1
+})
+
+
+
 
